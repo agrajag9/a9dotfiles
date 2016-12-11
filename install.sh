35b6757663996601fadd1b26f75e3171dc54bf97
agrajag9 <dan.kotowski@gmail.com>
2016-12-11 09:37:28 -0500

    Update install.sh
    This actually does something now. Definitely not safe, but it'll at least deploy everything nicely...

--- install.sh
+++ install.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 
-cp _bash_aliases ~/.bash_aliases
-cp _bash_logout ~/.bash_logout
-cp _bashrc ~/.bashrc
-cp _profile ~/.profile
-cp _tmux.conf ~/.tmux.conf
-cp _vimrc ~/.vimrc
-mkdir -p ~/.tmux/
-cp _tmux/* ~/.tmux/
-mkdir -p ~/.config/powerline/
-cp -r _config/powerline/* ~/.config/powerline/
+echo "This will delete the following files and directories,"
+echo "replacing them with symlinks to the files in this repo:"
+echo ""
+echo "~/.bashrc"
+echo "~/.bash_aliases"
+echo "~/.bash_logout"
+echo "~/.profile"
+echo "~/.vimrc"
+echo "~/.tmux.conf"
+echo "~/.tmux/"
+echo "~/.config/powerline/"
 
+PWD=$(pwd)
+
+rm -rf ~/.bashrc \
+       ~/.bash_aliases \
+       ~/.bash_logout \
+       ~/.profile \
+       ~/.tmux.conf \
+       ~/.vimrc \
+       ~/.tmux/* \
+       ~/.config/powerline/*
+
+ln -s $PWD/_bashrc ~/.bashrc
+ln -s $PWD/_bash_aliases ~/.bash_aliases
+ln -s $PWD/_bash_logout ~/.bash_logout
+ln -s $PWD/_profile ~/.profile
+
+mkdir -p ~/.tmux
+ln -s $PWD/_tmux/* ~/.tmux/
+ln -s $PWD/_tmux.conf ~/.tmux.conf
+
+mkdir -p ~/.config/powerline
+ln -s $PWD/_config/powerline/* ~/.config/powerline/
+
+ln -s $PWD/_vimrc ~/.vimrc
