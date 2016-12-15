77ae58a8beb34568ae880a409048611b589d5a50
agrajag9 <agrajag9@a9development.com>
2016-12-15 13:28:44 -0500

    Update install.sh
    Fixed and issue where install.sh would make circular symlinks after removing outdated files

--- install.sh
+++ install.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+#a9dotfiles/
+#├── _bash_aliases
+#├── _bash_logout
+#├── _bashrc
+#├── _config
+#│   └── powerline
+#│       ├── colorschemes
+#│       │   └── default.json
+#│       ├── config.json
+#│       ├── powerline.conf
+#│       └── themes
+#│           └── tmux
+#│               └── a9powerline.json
+#├── install.sh
+#├── LICENSE.md
+#├── _profile
+#├── README.md
+#├── _tmux
+#│   ├── mouse-off
+#│   └── mouse-on
+#├── _tmux.conf
+#└── _vimrc
+
 echo "This will delete the following files and directories,"
 echo "replacing them with symlinks to the files in this repo:"
 echo ""
@@ -20,19 +43,28 @@ rm -rf ~/.bashrc \
        ~/.profile \
        ~/.tmux.conf \
        ~/.vimrc \
-       ~/.tmux/* \
+       ~/.tmux/mouse-off \
+       ~/.tmux/mouse-on \
        ~/.config/powerline/*
 
+# Bash
 ln -s $PWD/_bashrc ~/.bashrc
 ln -s $PWD/_bash_aliases ~/.bash_aliases
 ln -s $PWD/_bash_logout ~/.bash_logout
 ln -s $PWD/_profile ~/.profile
 
+# Tmux
 mkdir -p ~/.tmux
-ln -s $PWD/_tmux/* ~/.tmux/
 ln -s $PWD/_tmux.conf ~/.tmux.conf
+find $PWD/_tmux -type f -exec ln -s {} ~/.tmux/ \;
 
-mkdir -p ~/.config/powerline
-ln -s $PWD/_config/powerline/* ~/.config/powerline/
+# Powerline
+mkdir -p ~/.config/powerline/colorschemes
+mkdir -p ~/.config/powerline/themes/tmux
+ln -s $PWD/_config/powerline/config.json ~/.config/powerline/
+ln -s $PWD/_config/powerline/powerline.conf ~/.config/powerline/
+ln -s $PWD/_config/powerline/colorschemes/default.json ~/.config/powerline/colorschemes/
+ln -s $PWD/_config/powerline/themes/tmux/a9powerline.json ~/.config/powerline/themes/tmux/
 
+# Vim
 ln -s $PWD/_vimrc ~/.vimrc
