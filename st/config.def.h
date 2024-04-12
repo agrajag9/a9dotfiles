25dce65b39023e38123be1152d95cd69f128aa1b
agrajag9 <agrajag9@a9development.com>
2024-04-12 05:27:18 +0000

    Font tuning

--- st/config.def.h
+++ st/config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SauceCodePro Nerd Font:pixelsize=13:antialias=true:autohit=true";
+static char *font = "SauceCodePro Nerd Font:pixelsize=12:antialias=true:autohit=true";
 static int borderpx = 2;
 
 /*
